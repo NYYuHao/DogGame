aaa9dbb69499327b04a8092aad512ff4eef86d64
NYYuHao <NYYuHao@gmail.com>
2019-10-20 04:55:01 +0000

    Added a bunch of styling and game mechanics

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 var money = 0;
 var speed = 1;
 var speedcost = 100;
+var tickPeriod = 1000;
+var dogs = 0;
+var dogcost = 1000;
+
+setInterval(gameLoop, tickPeriod);
 
 function increaseMoney() {
 	money += speed;
-	document.getElementById("moneycount").innerHTML = money;
+	updateCSS();
 }
 
 function increaseSpeed() {
@@ -12,8 +17,37 @@ function increaseSpeed() {
 		money -= speedcost;
 		speed += 1;
 		speedcost = Math.floor(speedcost*1.1);
-		document.getElementById("moneycount").innerHTML = money;
-		document.getElementById("speedCount").innerHTML = speed;
-		document.getElementById("buySpeed").innerHTML = `Increase money/click for ${speedcost} moneys`;
+		document.getElementById("buySpeed").innerHTML = `Increase money/click for ${speedcost} monies`;
+		updateCSS();
 	}
+}
+
+function increaseDogs() {
+	if (money >= dogcost) {
+		money -= dogcost;
+		dogs += 1;
+		dogcost = Math.floor(dogcost*1.5);
+		document.getElementById("buyDog").innerHTML = `Increase dogs for ${dogcost} monies`;
+		updateCSS();
+	}
+}
+
+function updateCSS() {
+	document.getElementById("moneycount").innerHTML = money;
+	document.getElementById("speedCount").innerHTML = speed;
+	document.getElementById("dogCount").innerHTML = dogs;
+	document.getElementById("tickCount").innerHTML = tickPeriod;
+	if (money >= speedcost)
+		document.getElementById("buySpeed").style = "opacity:1.0";
+	else
+		document.getElementById("buySpeed").style = "opacity:0.6";
+	if (money >= dogcost)
+		document.getElementById("buyDog").style = "opacity:1.0";
+	else
+		document.getElementById("buyDog").style = "opacity:0.6";
+}
+
+function gameLoop() {
+	money += speed * dogs;
+	updateCSS();
 }
